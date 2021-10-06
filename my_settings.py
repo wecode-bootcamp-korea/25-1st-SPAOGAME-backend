182d56024f275ab7e2e58b3c16ceccceb110d1be
song <song7868@gmail.com>
2021-10-06 11:47:28 +0900

    ADD:my_settings.py

---
+++ my_settings.py
@@ -0,0 +1,13 @@
+MY_DATABASES = {
+    'default' : {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'spaogame',
+        'USER': 'root',
+        'PASSWORD': '12341234',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+    }
+}
+
+MY_SECRET_KEY = 'django-insecure-ms-_=#vpn+9!%#ip_+%1_l4&odmgt0c^s9v^3=rv+tax*5eazd'
+MY_ALGORITMHS = 'HS256'
