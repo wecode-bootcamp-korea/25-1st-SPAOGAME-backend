8aa48ed34aeae08b96d7ec4fd1fa077241caf0f9
song <song7868@gmail.com>
2021-10-08 21:51:52 +0900

    로그인 구현

--- users/views.py
+++ users/views.py
@@ -53,7 +53,34 @@ class SignUpView(View):
         except KeyError:
             return JsonResponse({'MESSAGE':'KEY_ERROR'}, status=400)
 
+class SignInView(View):
+    def post(self, request):
+        try:
+            data = json.loads(request.body)
 
+            email          = data['email']
+            password       = data['password']
+
+            if not (email and password):
+                return JsonResponse({'MESSAGE':'EMPTY_VALUE'}, status=400)
+                
+            if not User.objects.filter(email=email).exists():
+                return JsonResponse({'MESSAGE':'USER_DOES_NOT_EXIST'}, status=401)
+            
+            user = User.objects.get(email=email)
+                
+            if not bcrypt.checkpw(data['password'].encode('utf-8'), user.password.encode('utf-8')):
+                return JsonResponse({'MESSAGE':'INVALID_USER'}, status=401)
+
+            token = jwt.encode({'id' : user.id}, MY_SECRET_KEY, MY_ALGORITHMS)
+            
+            return JsonResponse({'MESSAGE':'SUCCESS', 'TOKEN' : token}, status=200)
+
+        except KeyError:
+            return JsonResponse({'MESSAGE':'KEY_ERROR'}, status=400)
+        except ValueError:
+            return JsonResponse({'MESSAGE':'VALUE_ERROR'}, status=400)
+        
 
 
 
