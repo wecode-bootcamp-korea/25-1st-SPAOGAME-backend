72f6dcc0137ec60752720b6e083a8912cdbf85a1
kjhabc2002 <juhju2022@gmail.com>
2021-10-10 15:20:41 +0900

    fix : 후기조회 코드수정

--- postings/urls.py
+++ postings/urls.py
@@ -1,6 +1,7 @@
 from django.urls      import path
-from postings.views   import PostingView
+from postings.views   import PostingView, PostingListView
 
 urlpatterns = [
-    path('', PostingView.as_view())
+    path('', PostingView.as_view()),
+    path('<int:produt_id>', PostingListView.as_view())
 ]
--- postings/views.py
+++ postings/views.py
@@ -4,7 +4,8 @@ from django.views import View
 from django.http.response import JsonResponse
 
 from postings.models import Posting
-from products.models import  Image
+from users.models import User
+from products.models import  Image, Product
 from django.shortcuts import render
 
 
@@ -36,8 +37,31 @@ class PostingView(View):
             return JsonResponse({'message' : 'SUCCESS'}, status=201)
         
         except JSONDecodeError:
-            return JsonResponse({'message': 'JSON_DECODE_ERROR'}, status=400)
+            return JsonResponse({'message' : 'JSON_DECODE_ERROR'}, status=400)
         
         except KeyError :
-            return JsonResponse({'message':'KEYERROR'}, status=400)
+            return JsonResponse({'message' : 'KEYERROR'}, status=400)
 
+class PostingListView(View) :   
+    def get(self,request,product_id):    
+        try:
+            
+            if not Product.objects.filter(id=product_id).exists():
+                return JsonResponse ({'message':'PRODUCT_DOES_NOT_EXIST'}, status=404)
+            
+            posting_list = [{
+                    'title'     : posting.title,
+                    'user_id'   : posting.user_id,
+                    'username'  : User.objects.get(id=posting.user_id).username,
+                    'content'   : posting.content,
+                    'image_url' : [i.image_url for i in Image.objects.filter(posting_id=posting.id)]
+                }for posting in Posting.objects.filter(product_id=product_id)
+            ]
+            
+            
+            return JsonResponse({'data' : posting_list}, status=201)
+        except KeyError :
+            return JsonResponse({'message' : 'KEYERROR'}, status=400)
+        except ValueError:
+            return JsonResponse({'message' : 'DECODE_ERROR'}, status = 400) 
+    
