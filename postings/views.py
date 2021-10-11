b355bca32f78fc5cab68e1671d504f35544b2c5a
kjhabc2002 <juhju2022@gmail.com>
2021-10-11 18:01:08 +0900

    Fix : posting앱 제품후기 등록 기능 수정

--- postings/views.py
+++ postings/views.py
@@ -6,37 +6,39 @@ from django.http.response import JsonResponse
 from postings.models import Posting
 from products.models import  Image
 from django.shortcuts import render
-
+from users.decorators import login_decorator
+from django.db import transaction 
 
 class PostingView(View):
+    @login_decorator
     def post(self, request):       
         try:
             data        = json.loads(request.body)
-        
-            user_id     = data['user_id']
-            content     = data.get('content',None)
+         
+            user_id     = request.user.id
+            content     = data.get('content')
             title       = data['title']
             product_id  = data['product_id']
             urls        = data.get('urls',None)
         
-        
-            posting = Posting.objects.create(
-                user_id    = user_id,
-                content    = content,
-                title      = title,
-                product_id = product_id
-            )
-        
-            if urls :
-                for image_url in urls :
-                    Image.objects.create(
-                        image_url = image_url,
-                        posting   = posting
-                    ) 
+            with transaction.atomic():
+                posting = Posting.objects.create(
+                    user_id    = user_id,
+                    content    = content,
+                    title      = title,
+                    product_id = product_id
+                )
+                
+                if urls :
+                    for image_url in urls :
+                        Image.objects.create(
+                            urls         = image_url,
+                            posting_id   = posting.id
+                        ) 
             return JsonResponse({'message' : 'SUCCESS'}, status=201)
         
         except JSONDecodeError:
-            return JsonResponse({'message': 'JSON_DECODE_ERROR'}, status=400)
+            return JsonResponse({'message' : 'JSON_DECODE_ERROR'}, status=400)
         
         except KeyError :
-            return JsonResponse({'message':'KEYERROR'}, status=400)
+            return JsonResponse({'message' : 'KEYERROR'}, status=400)
