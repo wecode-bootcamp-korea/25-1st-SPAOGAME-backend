d4472ff42289785b44b50225d022bd677d2c19e2
song <song7868@gmail.com>
2021-10-12 10:53:30 +0900

    ADD: orders/models.py class of basket 'related_name'

--- orders/models.py
+++ orders/models.py
@@ -12,8 +12,8 @@ class Wishlist(TimeStampedModel) :
 class Basket(TimeStampedModel) :
     user        = models.ForeignKey('users.User', on_delete=models.CASCADE)
     product     = models.ForeignKey('products.Product', on_delete=models.CASCADE)
-    color       = models.ForeignKey('products.Color', on_delete=models.CASCADE)
-    size        = models.ForeignKey('products.Size', on_delete=models.CASCADE)    
+    color       = models.ForeignKey('products.DetailedProduct', related_name='basket_color', on_delete=models.CASCADE)
+    size        = models.ForeignKey('products.DetailedProduct', related_name='basket_size', on_delete=models.CASCADE)    
     quantity    = models.IntegerField(default=1)
 
     class Meta :
