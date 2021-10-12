836690bd3da2f99d2afd131535a368bb7cb87bfe
kjhabc2002 <juhju2022@gmail.com>
2021-10-13 03:24:21 +0900

    csv db uploader test

--- postings/views.py
+++ postings/views.py
@@ -48,28 +48,28 @@ class CommentView(View):
     @login_decorator
     def post(self, request):
             try:
-                data=json.loads(request.body)
-                user = request.user
+                data        = json.loads(request.body)
+                user        = request.user
                 
-                content = data['content']
-                posting_id = data['posting_id']
-                user_id = user.id
+                content     = data['content']
+                posting_id  = data['posting_id']
+                user_id     = user.id
                 
                 if not (content and posting_id):
                     return JsonResponse({'message':'KEY-ERROR'}, status=400)
                 
-                if not Posting.objects.filter(id=posting_id).exists():
-                    return JsonResponse({'message':"POSTING-DOES-NOT-EXIST"}, status=404)
+                if not Posting.objects.filter(id = posting_id).exists():
+                    return JsonResponse({'message' : "POSTING-DOES-NOT-EXIST"}, status=404)
                 
-                posting = Posting.objects.get(id=posting_id)
+                posting = Posting.objects.get(id = posting_id)
             
                 Comment.objects.create(
-                    content = content,
-                    user_id = user_id,
+                    content    = content,
+                    user_id    = user_id,
                     posting_id = posting.id
                 )
                 
-                return JsonResponse({'message':'SUCCESS'}, status=200)
+                return JsonResponse({'message' : 'SUCCESS'}, status=200)
             
             except JSONDecodeError:
-                return JsonResponse({'message':'JSON_DECODE_ERROR'}, status=400)
+                return JsonResponse({'message' : 'JSON_DECODE_ERROR'}, status=400)
