b1e7f23529511b55ef10beff68ffca3b7716b7e8
kjhabc2002 <juhju2022@gmail.com>
2021-10-13 12:58:23 +0900

    csv db uploader test

--- db_uploader.py
+++ db_uploader.py
@@ -3,47 +3,60 @@ import django
 import csv
 import sys
 
+from django.db import transaction
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "spao.settings")
 
 django.setup()
 
-from products.models import Menu, Category, Product, Color, Size
+from products.models import (
+    Product, 
+    Color, 
+    Size,
+    Menu,
+    Category
+)
 
 CSV_PATH_PRODUCTS = './spao_data.csv'
+with transaction.atomic() :
+    with open(CSV_PATH_PRODUCTS) as in_file :
 
-with open(CSV_PATH_PRODUCTS) as in_file :
-
-    data_reader = csv.reader(in_file)
-    
-    next(data_reader,None)
-
-    for row in data_reader :
+        data_reader = csv.reader(in_file)
         
-        if row[1] :
-            menu_name=row[1]
-            Menu.objects.create(name=menu_name)
-        
-        if row[2] :
-            category_name = row[2]
-            Category.objects.create(name=category_name, menu_id=Menu.objects.get(name=menu_name).id)
+        next(data_reader,None)
 
-        if row[3] :
-            product_name = row[3]
-        
-        if row[4] :
-            price = row[4]
-        
-        if row[5] :
-            description = row[5]
-        
-        if row[6] :
-            quantity = row[6]
-            Product.objects.create(name=product_name, price=price, description=description, quantity=quantity)
+        for row in data_reader :
+            
+            # if row[0] :
+            #     color_name=row[0]
+            #     Color.objects.create(name=color_name)
+            
+            # if row[1] :
+            #     size_name = row[1] 
+            #     Size.objects.create(name=size_name)
 
-        if row[7] : 
-            color_name = row[7]
-            Color.objects.create(name=color_name)
-
-        if row[8] :
-            size_name = row[8] 
-            Size.objects.create(name=size_name)
+            if row[3] :
+                product_name = row[3]
+            
+            if row[4] :
+                menu_name = row[4]
+                #Menu.objects.create(name=menu_name)
+                menu_id   = Menu.objects.get(name=menu_name).id
+            
+            if row[5] :
+                category_name = row[5]
+                #Category.objects.create(name=category_name, menu_id=menu_id)
+                category_id  = Category.objects.get(menu_id=menu_id, name=category_name).id
+            
+            # if row[6] :
+                price = row[6]
+            
+            if row[7] :
+                description = row[7]
+            
+            if row[8] :
+                quantity = row[8]
+            
+            if row[9] :
+                thumbnail = row[9]
+                Product.objects.create(name=product_name, menu_id=menu_id, category_id=category_id ,price=price, description=description, quantity=quantity, thumbnail_image_url=thumbnail)
