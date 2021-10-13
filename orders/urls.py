02f31b5eac08987d12ff76560145050d29ab56ed
song <song7868@gmail.com>
2021-10-13 18:07:47 +0900

    ADD: 장바구니 수정 기능 추가

--- orders/urls.py
+++ orders/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from orders.views import CartView
+from orders.views import CartView,CartEditView
 
 urlpatterns = [
     path('/cart', CartView.as_view()),
+    path('/cart/edit', CartEditView.as_view()),
 ]
--- orders/views.py
+++ orders/views.py
@@ -12,7 +12,7 @@ class CartView(View):
     def post(self, request):
         try:
             data       = json.loads(request.body)
-            detailed_product_id = data['product_id']
+            detailed_product_id = data['product']
             quantity   = data['quantity']
 
             if not DetailedProduct.objects.filter(id=detailed_product_id).exists():
@@ -49,6 +49,20 @@ class CartView(View):
 
         except KeyError as e:
             return JsonResponse({'MESSAGE': f'{e}'+'_KEY_ERROR'}, status=401)
+
+class CartEditView(View):
+    @login_decorator
+    def patch(self, request, basket_id):
+        try:
+            data     = json.loads(request.body)
+            quantity = data['quantity']
+            cart     = Basket.objects.filter(id=basket_id, user_id=request.user)
+
+            cart.update(quantity=quantity)
+            return JsonResponse({'MESSAGE':'SUCCESS'}, status=201)
+
+        except KeyError:
+            return JsonResponse({'MESSAGE':'KEY_ERROR'}, status=400)
 
     @login_decorator
     def delete(self, request, basket_id):
