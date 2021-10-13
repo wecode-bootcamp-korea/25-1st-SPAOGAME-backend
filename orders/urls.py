4e5f722541aa460bc6272aeea5bea689de606320
song <song7868@gmail.com>
2021-10-13 15:50:25 +0900

    [MODIFIED] URL

--- orders/urls.py
+++ orders/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
 
-from users.views import SignUpView, SignInView
+from orders.views import CartView
 
 urlpatterns = [
-    path('/cart', SignUpView.as_view()),
-    path('/', SignInView.as_view()),
+    path('/cart', CartView.as_view()),
 ]
